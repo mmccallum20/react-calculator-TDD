8226c846f5acd82627828f9856de1d5c1ad64252
Mary McCallum <mary.mccallum@os.uk>
2023-02-28 15:57:04 +0000

    Master branch is project before TDD added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import Calculator from '../Calculator/Calculator';
-import './App.css';
+import React from "react";
+import Calculator from "../Calculator/Calculator";
+import "./App.css";
 
-const App = () => (
-  <div className="app-container">
-    <Calculator />
-  </div>
-);
+const App = () => <div className="app-container">Hello World</div>;
 
 export default App;
