cb332f87b16d97fe65aa2f87ac44dcd433205929
Mary McCallum <mary.mccallum@os.uk>
2023-03-03 16:33:25 +0000

    Display element complete, tests passing

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Display from "../Display/Display";
+import "./Calculator.css";
 
 class Calculator extends Component {
   state = {
@@ -22,7 +24,13 @@ class Calculator extends Component {
   };
 
   render = () => {
-    return <div className="calculator-container" />;
+    const { displayValue } = this.state;
+
+    return (
+      <div className="calculator-container">
+        <Display displayValue={displayValue} />
+      </div>
+    );
   };
 }
 
--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Calculator from "./Calculator";
+import Display from "../Display/Display";
 
 describe("Calculator", () => {
   let wrapper;
 
-  //Calculator component below can be written as
-  //the component <Calculator /> or just using the word,
-  //VSCode will still recognise it.
+  beforeEach(() => (wrapper = shallow(<Calculator />)));
 
-  beforeEach(() => (wrapper = shallow(Calculator)));
+  it("should render a Display component", () => {
+    expect(
+      wrapper.containsMatchingElement(
+        <Display displayValue={wrapper.instance().state.displayValue} />
+      )
+    ).toEqual(true);
+  });
 
   it("should render a <div />", () => {
     expect(wrapper.find("div").length).toEqual(1);
